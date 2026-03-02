acb04eb83ee9f9a086937af15108fdad93d6b08b
bdaniek <bapaktheman@gmail.com>
2026-03-02 19:20:16 +0100

    vite fix

--- dashboard-frontend/vite.config.ts
+++ dashboard-frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vite.dev/config/
@@ -10,4 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  base: './',
+});
