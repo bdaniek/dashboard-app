bc68a3a704e1528a0e57ac4ac4eaa655b7001ae7
bdaniek <bapaktheman@gmail.com>
2026-03-02 20:53:23 +0100

    style fix

--- dashboard-frontend/src/components/WeeklyActivity/WeeklyActivityChart.styles.ts
+++ dashboard-frontend/src/components/WeeklyActivity/WeeklyActivityChart.styles.ts
@@ -14,7 +14,7 @@ export const Title = styled('div')`
 
 export const LoaderContainer = styled('div')`
   min-width: 100%;
-  min-height: 100vh;
+  min-height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
